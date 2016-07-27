ac4345783a7797fec64343272acb4b2b2097950f
Al Delcy <aldelcy2000@gmail.com>
2016-07-27 18:08:54 -0400

    setting up PG and Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Mael::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -65,5 +65,5 @@ Mael::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { host: 'maelbowtie.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'maelbowtie.herokuapp.com' }
 end
